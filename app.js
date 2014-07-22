a50a51f31ff721d743a0ab2f5c439b2d7c6a2e59
Evan <eulloyd@gmail.com>
2014-07-22 04:15:58 +0000

    Setup rough structure

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 angular.module('demo', [])
+.directive('makeEditable', function(){
+  return {
+    restrict: 'E',
+    templateUrl: './makeeditable.html',
+    transclude: true,
+    link: function(scope, element, attrs) {
+      scope.content = {
+        toggleEdit: function() {
+          console.log('clicked')
+        }
+      }
+    }
+  }
+})
